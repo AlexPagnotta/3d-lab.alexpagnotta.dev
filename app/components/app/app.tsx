9500a0d25e240735f8a5828bbd473e3361814fcf
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2024-01-04 14:52:49 +0100

    feat: add threeJS and  basic 3d scene

--- app/components/app/app.tsx
+++ app/components/app/app.tsx
@@ -1,7 +1,22 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 export const App = () => {
   return (
-    <>
-      <p className="text-black">3D LAB</p>
-    </>
+    <Canvas
+      shadows
+      camera={{
+        position: [0, 0, 5],
+      }}
+    >
+      <ambientLight intensity={1.5} />
+      <directionalLight position={[-2, 2, 3]} intensity={1.5} castShadow />
+
+      <OrbitControls />
+      <mesh castShadow receiveShadow rotation={[0, 2, 0]}>
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
+    </Canvas>
   );
 };
