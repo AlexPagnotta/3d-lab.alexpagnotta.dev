851d16a1e872a2d6a5e8e98154d00934577b760e
Alex Pagnotta <ap.alex.pagnotta@gmail.com>
2024-02-26 08:34:24 +0100

    feat: add improvements to scene

--- app/pages/scenes/ballpit.tsx
+++ app/pages/scenes/ballpit.tsx
@@ -1,4 +1,4 @@
-import { OrbitControls, OrthographicCamera } from "@react-three/drei";
+import { Box, Environment, OrbitControls, PerspectiveCamera, SpotLight } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
 import {
   CuboidCollider,
@@ -12,73 +12,105 @@ import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 import { Suspense, useEffect, useMemo, useRef, useState } from "react";
 
+import { useMediaQuery } from "~/features/dom/hooks/use-media-query";
+import { up } from "~/features/dom/utils/screens";
 import { getRandomNumber } from "~/features/utils/random";
+
+const CONTAINER_COLOR = "#ffffff";
+const SPHERES_COLOR = "#ffffff";
 
 const SPHERES_COUNT = 40;
 const SPHERE_RADIUS = 1;
 const SPHERES_GAP = SPHERE_RADIUS * 2;
 
 const PANELS_GAP = SPHERE_RADIUS * 2 + 0.3;
-const PANELS_THICKNESS = 2;
+const PANELS_THICKNESS = 0.5;
+const PANELS_THICKNESS_DESKTOP = 2;
 const PANELS_DEPTH = PANELS_GAP;
 
 const PANELS_OFFSET = SPHERES_GAP * SPHERES_COUNT; // How much the panels are offseted from the viewport edge in height
 
+type PanelPosition = "bottom" | "top" | "left" | "right" | "back" | "front";
+
+const ContainerPanels = (
+  width: number,
+  height: number,
+  thickness: number
+): Record<PanelPosition, { args: [number, number, number]; position: [number, number, number] }> => ({
+  bottom: {
+    args: [width + thickness * 2, thickness, PANELS_DEPTH],
+    position: [0, -thickness / 2, 0],
+  },
+  top: {
+    args: [width + thickness * 2, thickness, PANELS_DEPTH],
+    position: [0, height + thickness / 2, 0],
+  },
+  left: {
+    args: [thickness, height, PANELS_DEPTH],
+    position: [-(width / 2) - thickness / 2, height / 2, 0],
+  },
+  right: {
+    args: [thickness, height, PANELS_DEPTH],
+    position: [width / 2 + thickness / 2, height / 2, 0],
+  },
+  back: {
+    args: [width + thickness * 2, height, thickness],
+    position: [0, height / 2, -PANELS_DEPTH / 2 - thickness / 2],
+  },
+  front: {
+    args: [width + thickness * 2, height, thickness],
+    position: [0, height / 2, PANELS_DEPTH / 2 + thickness / 2],
+  },
+});
+
 const Container = () => {
+  const isMdUp = useMediaQuery(up("md"));
   const { width: viewportWidth, height: viewportHeight } = useThree((state) => state.viewport);
 
-  const panelsWidth = viewportWidth;
+  const panelsThickness = isMdUp ? PANELS_THICKNESS_DESKTOP : PANELS_THICKNESS;
+
+  const panelsWidth = viewportWidth - panelsThickness * 2;
   const panelsHeight = viewportHeight + PANELS_OFFSET;
 
   return (
-    <RigidBody type="fixed" colliders={false} position={[0, -viewportHeight / 2, 0]}>
+    <RigidBody type="fixed" colliders={false} position={[0, -viewportHeight / 2 + panelsThickness, 0]}>
       {/* Bottom */}
-      <CuboidCollider
-        scale={0.5}
-        args={[panelsWidth + PANELS_THICKNESS * 2, PANELS_THICKNESS, PANELS_DEPTH]}
-        position={[0, -PANELS_THICKNESS / 2, 0]}
-      />
+      <Box {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["bottom"]} receiveShadow>
+        <meshStandardMaterial color={CONTAINER_COLOR} />
+      </Box>
+      <CuboidCollider scale={0.5} {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["bottom"]} />
 
       {/* Top */}
-      <CuboidCollider
-        scale={0.5}
-        args={[panelsWidth + PANELS_THICKNESS * 2, PANELS_THICKNESS, PANELS_DEPTH]}
-        position={[0, panelsHeight + PANELS_THICKNESS / 2, 0]}
-      />
+      <CuboidCollider scale={0.5} {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["top"]} />
 
       {/* Left */}
-      <CuboidCollider
-        scale={0.5}
-        args={[PANELS_THICKNESS, panelsHeight, PANELS_DEPTH]}
-        position={[-(panelsWidth / 2) - PANELS_THICKNESS / 2, panelsHeight / 2, 0]}
-      />
+      <Box {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["left"]} receiveShadow>
+        <meshStandardMaterial color={CONTAINER_COLOR} />
+      </Box>
+      <CuboidCollider scale={0.5} {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["left"]} />
 
       {/* Right */}
-      <CuboidCollider
-        scale={0.5}
-        args={[PANELS_THICKNESS, panelsHeight, PANELS_DEPTH]}
-        position={[panelsWidth / 2 + PANELS_THICKNESS / 2, panelsHeight / 2, 0]}
-      />
+      <Box {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["right"]} receiveShadow>
+        <meshStandardMaterial color={CONTAINER_COLOR} />
+      </Box>
+      <CuboidCollider scale={0.5} {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["right"]} />
 
       {/* Back */}
-      <CuboidCollider
-        scale={0.5}
-        args={[panelsWidth + PANELS_THICKNESS * 2, panelsHeight, PANELS_THICKNESS]}
-        position={[0, panelsHeight / 2, -PANELS_DEPTH / 2 - PANELS_THICKNESS / 2]}
-      />
+      <Box {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["back"]} receiveShadow>
+        <meshStandardMaterial color={CONTAINER_COLOR} />
+      </Box>
+      <CuboidCollider scale={0.5} {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["back"]} />
 
       {/* Front */}
-      <CuboidCollider
-        scale={0.5}
-        args={[panelsWidth + PANELS_THICKNESS * 2, panelsHeight, PANELS_THICKNESS]}
-        position={[0, panelsHeight / 2, PANELS_DEPTH / 2 + PANELS_THICKNESS / 2]}
-      />
+      <CuboidCollider scale={0.5} {...ContainerPanels(panelsWidth, panelsHeight, panelsThickness)["front"]} />
     </RigidBody>
   );
 };
 
+Container.displayName = "Container";
+
 const Spheres = ({ isCameraReady }: { isCameraReady: boolean }) => {
-  const { enabled: animationEnabled, restitution } = useControls("Debug", {
+  const { enabled: animationEnabled, restitution } = useControls("Settings", {
     enabled: true,
     restitution: {
       min: 0,
@@ -130,9 +162,9 @@ const Spheres = ({ isCameraReady }: { isCameraReady: boolean }) => {
       mass={10}
       key={`spheres_${animationEnabled ? "enabled" : "disabled"}_${restitution}`}
     >
-      <instancedMesh args={[undefined, undefined, SPHERES_COUNT]} count={SPHERES_COUNT} castShadow receiveShadow>
+      <instancedMesh args={[undefined, undefined, SPHERES_COUNT]} count={SPHERES_COUNT} castShadow>
         <sphereGeometry args={[SPHERE_RADIUS, 32]} />
-        <meshStandardMaterial color="gray" />
+        <meshStandardMaterial color={SPHERES_COLOR} />
       </instancedMesh>
     </InstancedRigidBodies>
   );
@@ -142,7 +174,7 @@ const Scene = () => {
   const { debugView, perf, orbitControls, axesHelper } = useControls("Debug", {
     debugView: false,
     perf: false,
-    orbitControls: false,
+    orbitControls: true,
     axesHelper: false,
   });
 
@@ -152,18 +184,27 @@ const Scene = () => {
     <>
       {perf && <Perf position="top-left" />}
       {orbitControls && <OrbitControls />}
-
-      <OrthographicCamera
+      <PerspectiveCamera
         makeDefault={true}
         position={[0, 0, 50]}
-        zoom={50}
+        fov={20}
+        near={0.01}
+        far={500}
         onUpdate={() => {
           setIsCameraReady(true);
         }}
       />
       <ambientLight intensity={0.5} />
-      <directionalLight position={[1, 2, 3]} intensity={1.5} castShadow />
-
+      <Environment preset={"city"} />
+      <SpotLight
+        castShadow
+        intensity={500}
+        position={[0, 10, 10]}
+        angle={Math.PI / 3}
+        penumbra={0.5}
+        distance={50}
+        shadow-mapSize={2048}
+      />
       {axesHelper && (
         <axesHelper
           scale={2}
@@ -171,7 +212,6 @@ const Scene = () => {
           onUpdate={(self) => self.setColors("#ff2080", "#20ff80", "#2080ff")}
         />
       )}
-
       <Physics debug={debugView}>
         <Container />
         <Spheres isCameraReady={isCameraReady} />
